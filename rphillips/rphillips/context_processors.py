822b8ad2dbf9fddf66553de9e83798046ce022d0
rr-phillips <rebecca@rphillips.co>
2021-01-23 21:54:14 -0500

    updated html files

--- rphillips/rphillips/context_processors.py
+++ rphillips/rphillips/context_processors.py
@@ -2,6 +2,7 @@ from .settings import BASE_DIR
 
 def baseProcessor(request):
     context = {
-        'base_template': BASE_DIR / 'templates/base_template.html'
+        'base_template': BASE_DIR / 'templates/base_template.html',
+        'base_dir': BASE_DIR
     }
     return context
