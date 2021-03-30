7868b7f064a9594dda1e59d86ee306e702027787
rr-phillips <rebecca@rphillips.co>
2021-03-29 22:17:20 -0400

    updated layout to single page layout of vertical scrolling

--- rphillips/home/views.py
+++ rphillips/home/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from projects.models import Projects
+from resume.models import Job, Education
 
 # Create your views here.
 def index(request):
     template = "home/home_index.html"
-    return render(request, template)
+    allProjects = Projects.objects.all()
+    allJobs = Job.objects.all().order_by('start')
+    allEducation = Education.objects.all().order_by('start')
+
+    context = {
+        'projects' : allProjects,
+        'jobs': allJobs,
+        'educations': allEducation,
+    }
+    return render(request, template, context)
