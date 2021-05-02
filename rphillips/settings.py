c7296b9d09c1080474d4783fddfdec04ef345005
rr-phillips <rebecca@rphillips.co>
2021-05-02 16:07:43 -0400

    updated the settings and styling

--- rphillips/settings.py
+++ rphillips/settings.py
@@ -21,7 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%sok^*i+m&&4o6%p=#1*^+zm9nzw$bum!6w^6hdu=3h^ju*o7m'
+try: 
+    SECRET_KEY = os.environ['secret_key']
+except:
+    with open('rphillips/secret_key.txt') as f:
+        SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
